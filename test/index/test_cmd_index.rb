2e62ea38c516791ef82cd2593d6f61cbe61d62fa
ShinYee <shinyee@speedgocomputing.com>
2015-06-29 09:11:38 +0800

    Getting started with tests for `fm` and `fm index`.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

---
+++ test/index/test_cmd_index.rb
@@ -0,0 +1,20 @@
+require 'test/unit'
+
+require 'fm'
+
+
+class TestCmdIndex < Test::Unit::TestCase
+
+    def setup
+    end
+
+
+    def teardown
+    end
+
+
+    def test_this_test
+        assert(true)
+    end
+
+end
---
+++ test/test_fm.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+
+
+class TestFM < Test::Unit::TestCase
+
+    def setup
+    end
+
+
+    def teardown
+    end
+
+
+    def test_this_test
+        assert(true)
+    end
+
+end
