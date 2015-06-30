a52d447c91eb7288cef19dd3f7ba3fc44c438e92
ShinYee <shinyee@speedgocomputing.com>
2015-07-01 01:01:13 +0800

    Record last index time.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

--- lib/fm/file.rb
+++ lib/fm/file.rb
@@ -5,12 +5,14 @@ module FM
         attr_reader :digest
         attr_reader :path
         attr_accessor :mtime
+        attr_accessor :itime
 
-        def initialize(fsize, path, digest, mtime)
+        def initialize(fsize, path, digest, mtime, itime = Time.now)
             @fsize = fsize
             @digest = digest
             @path = path
             @mtime = mtime
+            @itime = itime      # Last index time.
         end
     end
 
